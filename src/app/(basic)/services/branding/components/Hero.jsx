f3948a904b4e0d205d9ce151b674493eed1dd6f6
Raheel Arfeen <raheelarfeen@gmail.com>
2026-04-13 18:07:54 +0600

    updated the UI UX page

--- src/app/(basic)/services/branding/components/Hero.jsx
+++ src/app/(basic)/services/branding/components/Hero.jsx
@@ -27,12 +27,11 @@ const Hero = () => {
             <Image className='absolute max-w-[950px] w-full -right-40 -bottom-60 z-10  opacity-50' src={rightCube} alt="" />
 
 
-            <section className='z-20 text-center pt-30 mb-40 px-4'>
-                <h3 className='outfit uppercase text-[#C7C7C7] font-bold text-sm md:text-lg mb-3 tracking-[2px]'>Home / Services</h3>
-                <h1 className="text-[36px] sm:text-[48px] md:text-[50px] font-semibold text-[#C9FF90] bricolage leading-[1.1] md:leading-[60px] mb-4 tracking-tight">Branding & Identity</h1>
-                <p className='outift text-2xl text-white/79 tracking-tight max-w-[520px] mb-9'>We’re a team of designers and developers with over a decade of experience helping brands grow.</p>
+            <section className='z-20 text-center pt-30 mb-40 px-4 flex flex-col items-center justify-center'>
+                <h3 className='outfit uppercase text-[#C7C7C7] font-medium text-sm mb-4 tracking-[1px] py-2 px-4 bg-white/10 rounded-full w-fit space-x-1'><span>Home</span> <span>/</span> <span>Services</span> <span>/</span> <span className='text-white font-semibold'>Branding & Identity </span></h3>                <h1 className="text-[36px] sm:text-[48px] md:text-[50px] font-semibold text-[#C9FF90] bricolage leading-[1.1] md:leading-[60px] mb-4 tracking-tight">Branding & Identity</h1>
+                <p className='outift text-2xl text-white/79 tracking-tight max-w-[520px] mb-8'>We’re a team of designers and developers with over a decade of experience helping brands grow.</p>
 
-                <div className='flex flex-col md:flex-row items-center justify-center gap-3 mt-12 px-10'>
+                <div className='flex flex-col md:flex-row items-center justify-center gap-3 px-10'>
                     <button className='py-3 px-10 rounded-full bg-[#C9FF90] text-[#07302C] hover:bg-[#07302C] hover:text-[#C9FF90] transition duration-300 border-b border-[#C9FF90] hover:border-[#4d6641] outfit md:text-xl text-base  max-[350px]:text-xs cursor-pointer'>
                         <a href="https://calendly.com/teamorbillo/30min" target="_blank" rel="noopener noreferrer">Start A Project</a>
                     </button>
